9ff88deabb7dc9610dd1c57fa575d62eb45abb25
MinaoSyn <minaosyn@gmail.com>
2026-04-29 12:54:08 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# config.py
+BOT_TOKEN = "8601362165:AAEoOfTZAXUhOEXA4ZeJ39oLllP7JXhJzcw"  # Получи у @BotFather
+CHECK_INTERVAL = 30  # секунды между проверкой онлайна
+
+# Список скинов DDNet (можно дополнять)
+SKINS = [
+    "default", "x_ninja", "x_spec", "tee", "cammo",
+    "penguin", "brownbear", "bluekitty", "coala", "santa",
+    "twinbop", "greyfox", "red_ninja", "spiky", "warpaint",
+]
+
